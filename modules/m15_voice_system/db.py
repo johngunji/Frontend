94f79467a08596691347839506a93174f2daa0e3
John Gunji <johngunji05@gmail.com>
2026-03-19 19:59:30 +0530

    Add MySQL connection function using pymysql

--- modules/m15_voice_system/db.py
+++ modules/m15_voice_system/db.py
@@ -1,1 +1,11 @@
 
+import pymysql
+
+def get_connection():
+    return pymysql.connect(
+        host="localhost",
+        user="appuser",
+        password="app123",
+        database="m15",
+        cursorclass=pymysql.cursors.DictCursor
+    )
