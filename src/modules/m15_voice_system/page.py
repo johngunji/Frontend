34f7c8e1bd7e68bbd679ee9b6c1ae1e51499bc55
John Gunji <johngunji05@gmail.com>
2026-03-19 19:52:04 +0530

    Delete src/modules/m15_voice_system directory

--- src/modules/m15_voice_system/page.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-from modules.c3_voice_system import run_voice_system
-
-def render_page():
-    st.header("M15 - Voice-Assisted Clinical Query System")
-    run_voice_system()
