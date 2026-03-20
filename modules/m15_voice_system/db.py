f33c9a887de2c22ab7c159c60ee30cf86c7af0d9
John Gunji <johngunji05@gmail.com>
2026-03-20 07:16:33 +0530

    Update database connection parameters

--- modules/m15_voice_system/db.py
+++ modules/m15_voice_system/db.py
@@ -2,9 +2,10 @@ import pymysql
 
 def get_connection():
     return pymysql.connect(
-        host="localhost",
-        user="root",          # use root for now (avoid appuser issues)
-        password="root123",  # put your actual MySQL password
-        database="m15",
+        host="ballast.proxy.rlwy.net",
+        user="root",
+        password="YOUR_PASSWORD",
+        database="railway",
+        port=14747,
         cursorclass=pymysql.cursors.DictCursor
     )
